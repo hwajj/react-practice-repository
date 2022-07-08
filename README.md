3d537211d512c5f20aa3693ab015d01116350e8f
Jeong hwajeong <azamwara@gmail.com>
2022-07-08 22:50:45 +0900

    learn react useState

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# 리액트를 다루는 기술 따라하기
+# 리액트 공부 기록 
 
-## 3장 컴포넌트
-### props 
-
+## Props
 - props 기본값 설정 : defaultProps
     - defaultProps가 설정되지 않았는데 isRequired사용된 필수 propTypes일때 console에 경고  
 - 태그사이의 내용을 보여주는 children
@@ -11,3 +9,4 @@
 - class 컴포넌트의 constructor에서 super(props) 호출
     -> 리액트의 Component클래스가 지닌 생성자 함수를 호출함
     
+
--- src/App.js
+++ src/App.js
@@ -1,15 +1,81 @@
+import React, { useState } from 'react';
 import './App.css';
-import MyComponent from './MyComponent';
-import MyClassComponent from './MyClassComponent';
+
 function App() {
-	const name = '리액트';
+	let [blogList, setBlogList] = useState([
+		'미디어쿼리를 배워보자',
+		'Git revert로 커밋 되돌리기',
+		'Git reset으로 커밋 삭제하기',
+	]);
+	let [like, setLike] = useState(0);
 	return (
 		<>
-			<MyComponent name="컴포넌트">앱으로 리턴되라 얍 !</MyComponent>
-			<MyClassComponent name="클래스컴포넌트">
-				이건 클래스컴포넌트 사이에 들어간 글자고, props에 children으로 전달된다.
-			</MyClassComponent>
+			<div className="blog">
+				<div className="black-nav">
+					<h4>My Blog</h4>
+					<div></div>
+				</div>
+				<div className="button-container">
+					<button
+						onClick={() => {
+							let copy = [...blogList];
+
+							copy.sort();
+							setBlogList(copy);
+						}}
+					>
+						가나다 순 정렬 버튼
+					</button>
+
+					<button
+						onClick={() => {
+							let copy = [...blogList];
+							copy[0] = '부트스트랩으로 반응형 웹디자인만들기';
+							setBlogList(copy);
+						}}
+					>
+						버튼
+					</button>
+				</div>
+
+				<div className="lists">
+					<div className="list">
+						<h4>
+							{blogList[0]}
+							<span
+								onClick={() => {
+									setLike(like + 1);
+								}}
+							>
+								👍
+							</span>{' '}
+							{like}
+						</h4>
+						<p>2022/06/05</p>
+					</div>
+
+					<div className="list">
+						<h4>{blogList[1]}</h4>
+						<p>2022/06/03</p>
+					</div>
+					<div className="list">
+						<h4>{blogList[2]}</h4>
+						<p>2022/06/01</p>
+					</div>
+				</div>
+				<Modal></Modal>
+			</div>
 		</>
+	);
+}
+
+function Modal() {
+	return (
+		<div className="modal h-100">
+			<h4>제목</h4>
+			<p>날짜</p>
+			<p>상세내용</p>
+		</div>
 	);
 }
 
