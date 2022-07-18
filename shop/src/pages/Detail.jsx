c6c80800886293d714c92cd8c22988510d84bdbd
Jeong hwajeong <azamwara@gmail.com>
2022-07-19 03:34:19 +0900

    Use useEffect in Disappearing box

--- shop/src/pages/Detail.jsx
+++ shop/src/pages/Detail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -11,15 +11,44 @@ let StyledBtn = styled.button`
   border: 1px solid ${(props) => props.color};
   width: 100px;
 `;
+
+let DiscountBox = styled.div`
+  margin: 20px;
+  color: white;
+  font-size: 30px;
+  box-shadow: 2px 2px 2px black;
+  text-align: center;
+  padding-top: 23px;
+  background: lightsteelblue;
+  width: 30%;
+  height: 100px;
+`;
 function Detail(props) {
+  let [hide, setHide] = useState(false);
+  let [count, setCount] = useState(2);
+
+  useEffect(() => {
+    console.log('안녕');
+
+    setTimeout(() => {
+      setHide(true);
+    }, 2000);
+  });
+
+  setTimeout(() => {
+    setCount(1);
+  }, 1000);
+
   let { id } = useParams();
-  console.log(id);
   let shoesList = props.shoes;
-  console.log(shoesList);
   const shoes = shoesList.filter((shoes) => shoes.id == id)[0];
-  console.log(shoes);
   return (
     <div className='container'>
+      {hide == false ? (
+        <DiscountBox className='discountBox block'>
+          {count} 초 이내 구입 시 할인
+        </DiscountBox>
+      ) : null}
       <div className='row'>
         <div className='col-md-6'>
           <img
@@ -31,7 +60,15 @@ function Detail(props) {
           <h4 className='pt-5'>{shoes.title}</h4>
           <p>{shoes.detail}</p>
           <p>{shoes.price}원</p>
-          <StyledBtn bg='white' color='blue'>
+
+          <StyledBtn
+            bg='white'
+            color='blue'
+            onClick={() => {
+              console.log('재렌더링');
+              setCount(count + 1);
+            }}
+          >
             장바구니
           </StyledBtn>
           <StyledBtn bg='blue' color='white'>
@@ -39,7 +76,6 @@ function Detail(props) {
           </StyledBtn>
         </div>
       </div>
-      <div></div>
     </div>
   );
 }
