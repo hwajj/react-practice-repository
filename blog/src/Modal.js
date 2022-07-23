35b5844e46032be11f031b708a18e9c4a856139b
Jeong hwajeong <azamwara@gmail.com>
2022-07-24 03:02:27 +0900

    Add portal for modal components

--- blog/src/Modal.js
+++ blog/src/Modal.js
@@ -1,16 +1,20 @@
 import classes from './Modal.module.css';
+import ReactDOM from 'react-dom';
 
 function Modal(props) {
 	return (
 		<>
-			<>
-				<div className={classes.overlay} onClick={props.onCheck} />
-				<div className={`${classes.modal} ${classes['h-100']}`}>
-					<h4>{props.blogList[props.clickedIdx]}</h4>
-					<p>날짜</p>
-					<p>내용</p>
-				</div>
-			</>
+			{ReactDOM.createPortal(
+				<>
+					<div className={classes.overlay} onClick={props.onCheck} />
+					<div className={`${classes.modal} ${classes['h-100']}`}>
+						<h4>{props.blogList[props.clickedIdx]}</h4>
+						<p>날짜</p>
+						<p>내용</p>
+					</div>
+				</>,
+				document.getElementById('modal-root')
+			)}
 		</>
 	);
 }
