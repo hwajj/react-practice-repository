49dccdb90bb4d1cdb44bec788f345de85bdc0c5c
Jeong hwajeong <azamwara@gmail.com>
2022-08-30 09:35:21 +0900

    Context API tutorial

---
+++ context-tutorial/src/components/ColorBox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ColorContext from '../contexts/color';
+
+const ColorBox = () => {
+  return (
+    <ColorContext.Consumer>
+      {
+        //Render Props - 컴포넌트의 children이 있어야 할 자리에 함수 전달
+        (value) => (
+          <div
+            style={{
+              width: '64px',
+              height: '64px',
+              background: value.color,
+            }}
+          />
+        )
+      }
+    </ColorContext.Consumer>
+  );
+};
+
+export default ColorBox;
